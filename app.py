908dbce491de9675e10ed478d9c504191184f2c2
gms1k <bogdan.kripchenko@gmail.com>
2025-06-25 22:20:29 +0300

    демонстрація автоматичного деплою

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ async def read_root(request: Request):
     """
     context = {
         "request": request,
-        "title": "Мій оновлений DevOps-проект з FastAPI",
+        "title": "Мій фінальний DevOps-проект з FastAPI",
         "content": "Це оновлена демо-сторінка мого DevOps CI/CD конвеєра з додатковими деталями!",
         "author": "Кріпченко Богдан",
         "year": 2025
